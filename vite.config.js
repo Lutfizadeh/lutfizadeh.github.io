d960bc19b76a8d78817ba6b6133665ae4faa61f7
Lutfizadeh <lutfizadeh5@gmail.com>
2025-09-15 14:08:28 +0700

    Update title

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Lutfizadeh/',
   plugins: [react()],
   esbuild: {
     // Disable eslint warnings untuk framer-motion
